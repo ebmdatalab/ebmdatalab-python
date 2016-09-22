0fa7649454dd77f5a9b9bc191cce04967db62ba9
Seb Bacon <seb.bacon@gmail.com>
2016-09-22 09:43:45 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+EBMDataLab bigquery utils
+=========================
+
+Useful, common utilities for working with bigquery.
+
+This is currently a moving target and subject to fast, breaking
+changes...
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+REQUIREMENTS = []
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ebmdatalab-bigquery-tools',
+    version='0.0.1',
+    description='Tools used by EBMDataLab when interacting with BigQuery',
+    long_description=long_description,
+    url='https://github.com/ebmdatalab-bigquery-tools',
+    author='Seb Bacon',
+    author_email='tech@ebmdatalab.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='bigquery utilities',
+    packages=find_packages(),
+    # On the next release of gcloud we can move this to REQUIREMENTS
+    dependency_links=[
+        ('https://github.com/GoogleCloudPlatform/google-cloud-python'
+         '/tarball/master#egg=gcloud')],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': ['check-manifest'],
+    },
+)
