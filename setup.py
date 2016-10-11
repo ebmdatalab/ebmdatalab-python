25e03cf29e1fe19ef2d78fde7eae88d2e33f47d6
Seb Bacon <seb.bacon@gmail.com>
2016-10-11 09:59:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ebmdatalab-python',
-    version='0.0.3',
+    version='0.0.4',
     description='Tools used by EBMDataLab when interacting with BigQuery',
     long_description=long_description,
     url='https://github.com/ebmdatalab-tools',
